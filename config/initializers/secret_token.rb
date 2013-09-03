39f9c71ad5249d0996e5919259af6a93080bca7b
tito matata <cryptopascal@gmail.com>
2013-09-03 08:36:12 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vitatechpharma::Application.config.secret_token = 'c92c197dccc6bd8465ec3c4afc5d8ca41055e0b330c2d980d691d739857d25ac9feda2422cdcbbc77968e9e06271cc28eb7356d468e780ae2c862deb4a17a142'
